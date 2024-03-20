bf3b94319f51ad1c555302ed14a5882eade08be0
市石洋平 <yy_ichi@yahoo.co.jp>
2024-03-20 19:22:15 +0900

    Docker環境を構築

--- app.py
+++ app.py
@@ -122,4 +122,4 @@ def get_insert_value(image_path, image_info, request_timestamp, response_timesta
 	return insert_value
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run(debug=True, host ='0.0.0.0')
